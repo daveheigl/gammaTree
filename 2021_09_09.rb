fa19e6a4f2d22b3676cac9ccb6d71c6d0f16892d
David Heigl <david.heigl@gmail.com>
2021-10-09 00:05:52 -0500

    hash mostly works, but three letters aren't matching up, so the total isn't correct yet

--- 2021_09_09.rb
+++ 2021_09_09.rb
@@ -5,12 +5,19 @@
 
 #pulled value data from https://www.hebcal.com/home/1824/numerical-values-of-hebrew-letters
 puts "start of program"
+letterToNumber = Hash.new
 
 File.readlines('gamValues.csv', chomp:true).each do |line|
    puts "#{line}"
+   localArray = line.split(",");
+   if(localArray.count == 2)
+      letterToNumber[localArray[0]] = localArray[1];
+      puts "Hash contains #{letterToNumber.count}"
+   end
 end
 
 puts "/n --- /n"
+
 
 inputFile = File.open("gen1-1.txt", "r:utf-8")
 textLines = inputFile.readlines
@@ -20,12 +27,16 @@ inputFile.close
 puts "Counted #{text.length} raw characters in gen 1:1\n\n";
 
 i = 1
+textTotal = 0
+
 for b in text.chars do
 
      if b >= "א"
-          puts "#{b} #{i}"
+          puts "#{b} #{i} #{letterToNumber[b]}"
+          textTotal += letterToNumber[b].to_i;
           i+=1
      end
 end
 
 puts "Counted #{i-1} non-punctuation characters in gen 1:1\n\n";
+puts "Total counted value: #{textTotal}"
